e0095b2074c79c8a48fc8f9a6a513605e47953f0
Andz21 <149726596+Andz21@users.noreply.github.com>
2025-06-07 23:48:37 +0200

    Oop exercises

---
+++ OOP exercises/1_exercise.py
@@ -0,0 +1,12 @@
+# OOP Exercise 1: Create a Class with instance attributes
+# Write a Python program to create a Vehicle class with max_speed and mileage instance attributes.
+
+class Vehicle:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+        print("Yes")
+
+
+v = Vehicle(240,18)
+print(v.max_speed,v.mileage)
---
+++ OOP exercises/4_exercise.py
@@ -0,0 +1,16 @@
+# 4. Create a Bus class that inherits from the Vehicle class. Give the capacity
+# argument of Bus.seating_capacity() a default value of 50.
+
+class Vehicle:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+    def seating_capacity(self, capacity):
+        return f"The seating capacity of a {self.name} is {capacity} passengers"
+
+class Bus(Vehicle):
+    def __init__(self,name,max_speed, mileage,seating_capacity):
+        super().__init__()
+        self.seating_capacity = seating_capacity
